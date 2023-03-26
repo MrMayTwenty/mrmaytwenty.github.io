c54bab005e8ac754f49f247f066447f9d4ebc775
MrMayTwenty <30350668+MrMayTwenty@users.noreply.github.com>
2023-03-26 13:58:26 +0800

    added sample buttons

---
+++ Scripts/EventHandler.js
@@ -0,0 +1,32 @@
+function addInitialEventHandlers(){
+    document.addEventListener("DOMContentLoaded", function() {
+        const sceneEl = document.querySelector('a-scene');
+        const imgTarget = document.querySelector('#imgTarget');
+        const examplePlane = document.querySelector('#example-plane');
+        // arReady event triggered when ready
+        sceneEl.addEventListener("arReady", (event) => {
+          // console.log("MindAR is ready")
+        });
+        // arError event triggered when something went wrong. Mostly browser compatbility issue
+        sceneEl.addEventListener("arError", (event) => {
+          // console.log("MindAR failed to start")
+        });
+        // detect target found
+        imgTarget.addEventListener("targetFound", event => {
+          console.log("target found");
+          onTargetFound()
+        });
+        // detect target lost
+        imgTarget.addEventListener("targetLost", event => {
+          console.log("target lost");
+        });
+        // detect click event
+        examplePlane.addEventListener("click", event => {
+          console.log("plane click");
+        });
+    });
+}
+
+function onTargetFound(){
+
+}
