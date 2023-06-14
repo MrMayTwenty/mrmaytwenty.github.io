b9d30b11578ebf7399b7dace1e29113d25b0f666
MrMayTwenty <30350668+MrMayTwenty@users.noreply.github.com>
2023-06-14 20:46:44 +0800

    Update EventHandler.js

--- Scripts/EventHandler.js
+++ Scripts/EventHandler.js
@@ -1,40 +1,41 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   addInitialEventHandlers();
 }, false);
 
-function addInitialEventHandlers(){
-        const sceneEl = document.querySelector('a-scene');
-        const imgTarget = document.querySelector('#imgTarget');
-        const examplePlane = document.querySelector('#example-plane');
-        const btnPrevious = document.querySelector('#btnPrevious');
-        
-        // arReady event triggered when ready
-        sceneEl.addEventListener("arReady", (event) => {
-          // console.log("MindAR is ready")
-        });
-        // arError event triggered when something went wrong. Mostly browser compatbility issue
-        sceneEl.addEventListener("arError", (event) => {
-          // console.log("MindAR failed to start")
-        });
-        // detect target found
-        imgTarget.addEventListener("targetFound", event => {
-          console.log("target found");
-          onTargetFound()
-        });
-        // detect target lost
-        imgTarget.addEventListener("targetLost", event => {
-          console.log("target lost");
-        });
-        // detect click event
-        examplePlane.addEventListener("click", event => {
-          console.log("plane click");
-        });
+function addInitialEventHandlers() {
+  const sceneEl = document.querySelector('a-scene');
+  const imgTarget = document.querySelector('#imgTarget');
+  const examplePlane = document.querySelector('#example-plane');
+  const btnPrevious = document.querySelector('#btnPrevious');
 
-        btnPrevious.addEventListener("click", event => {
-          document.querySelector('#prototype-video').play();
-        });
+  // arReady event triggered when ready
+  sceneEl.addEventListener("arReady", (event) => {
+    // console.log("MindAR is ready")
+  });
+  // arError event triggered when something went wrong. Mostly browser compatbility issue
+  sceneEl.addEventListener("arError", (event) => {
+    // console.log("MindAR failed to start")
+  });
+  // detect target found
+  imgTarget.addEventListener("targetFound", event => {
+    console.log("target found");
+    onTargetFound()
+  });
+  // detect target lost
+  imgTarget.addEventListener("targetLost", event => {
+    console.log("target lost");
+  });
+  // detect click event
+  examplePlane.addEventListener("click", event => {
+    console.log("plane click");
+  });
+
+  btnPrevious.addEventListener("click", event => {
+    console.log("btnPrevious clicked")
+    document.querySelector('#prototype-video').play();
+  });
 }
 
-function onTargetFound(){
+function onTargetFound() {
 
 }
