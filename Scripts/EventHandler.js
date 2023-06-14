25ffc7ee866e6d6a24b34cb99d4ff6a2da43da94
MrMayTwenty <30350668+MrMayTwenty@users.noreply.github.com>
2023-06-14 21:27:31 +0800

    Update EventHandler.js

--- Scripts/EventHandler.js
+++ Scripts/EventHandler.js
@@ -47,6 +47,12 @@ function addInitialEventHandlers() {
     const directionLink = "https://www.google.com/maps/dir/Iglesia+Ni+Cristo+-+Lokal+ng+EVM+-+Cavite+South,+4W5X%2BXM5,+Tagaytay,+Cavite/LieSil%E2%80%99s+Restaurant+%26+Events,+J.P.+Rizal+Street,+Kaybagal+South,+Tagaytay,+Cavite/@14.1056229,120.9392312,15z/data=!4m14!4m13!1m5!1m1!1s0x33bd7744d1349a79:0x922c3e148452585b!2m2!1d120.9492484!2d14.1098942!1m5!1m1!1s0x33bd77b43a3e77a3:0xeebb99ef08880fc4!2m2!1d120.9360291!2d14.102883!3e0?entry=ttu";
     window.open(directionLink, "_blank");
   });
+
+  btnSponsors.addEventListener("click", event => {
+    console.log("btnSponsors clicked")
+    const directionLink = "https://www.google.com/maps/dir/Iglesia+Ni+Cristo+-+Lokal+ng+EVM+-+Cavite+South,+4W5X%2BXM5,+Tagaytay,+Cavite/LieSil%E2%80%99s+Restaurant+%26+Events,+J.P.+Rizal+Street,+Kaybagal+South,+Tagaytay,+Cavite/@14.1056229,120.9392312,15z/data=!4m14!4m13!1m5!1m1!1s0x33bd7744d1349a79:0x922c3e148452585b!2m2!1d120.9492484!2d14.1098942!1m5!1m1!1s0x33bd77b43a3e77a3:0xeebb99ef08880fc4!2m2!1d120.9360291!2d14.102883!3e0?entry=ttu";
+    window.open(directionLink, "_blank");
+  });
 }
 
 function onTargetFound() {
