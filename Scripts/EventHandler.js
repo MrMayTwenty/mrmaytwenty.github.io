e327a8e037baeb6c87c620cda9fa71444106d73e
MrMayTwenty <30350668+MrMayTwenty@users.noreply.github.com>
2023-06-14 21:08:27 +0800

    added interactions

--- Scripts/EventHandler.js
+++ Scripts/EventHandler.js
@@ -6,6 +6,9 @@ function addInitialEventHandlers() {
   const sceneEl = document.querySelector('a-scene');
   const imgTarget = document.querySelector('#imgTarget');
   const btnPrevious = document.querySelector('#btnPrevious');
+  const btnNext = document.querySelector('#btnNext');
+  const btnMap = document.querySelector('#btnMap');
+  const btnSponsors = document.querySelector('#btnSponsors');
 
   // arReady event triggered when ready
   sceneEl.addEventListener("arReady", (event) => {
@@ -28,6 +31,17 @@ function addInitialEventHandlers() {
   btnPrevious.addEventListener("click", event => {
     console.log("btnPrevious clicked")
     document.querySelector('#prototype-video').play();
+  });
+
+  btnNext.addEventListener("click", event => {
+    console.log("btnNext clicked")
+    document.querySelector('#prototype-video').play();
+  });
+
+  btnMap.addEventListener("click", event => {
+    console.log("btnMap clicked")
+    const directionLink = "https://www.google.com/maps/dir/Iglesia+Ni+Cristo+-+Lokal+ng+EVM+-+Cavite+South,+4W5X%2BXM5,+Tagaytay,+Cavite/LieSil%E2%80%99s+Restaurant+%26+Events,+J.P.+Rizal+Street,+Kaybagal+South,+Tagaytay,+Cavite/@14.1056229,120.9392312,15z/data=!4m14!4m13!1m5!1m1!1s0x33bd7744d1349a79:0x922c3e148452585b!2m2!1d120.9492484!2d14.1098942!1m5!1m1!1s0x33bd77b43a3e77a3:0xeebb99ef08880fc4!2m2!1d120.9360291!2d14.102883!3e0?entry=ttu";
+    window.open(directionLink, "_blank");
   });
 }
 
