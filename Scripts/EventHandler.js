b72f353036fa075540ca7d2f2c29817fe3f1edc8
MrMayTwenty <30350668+MrMayTwenty@users.noreply.github.com>
2023-06-14 21:41:00 +0800

    added more icons

--- Scripts/EventHandler.js
+++ Scripts/EventHandler.js
@@ -7,7 +7,8 @@ function addInitialEventHandlers() {
   const imgTarget = document.querySelector('#imgTarget');
   const btnPrevious = document.querySelector('#btnPrevious');
   const btnNext = document.querySelector('#btnNext');
-  const btnMap = document.querySelector('#btnMap');
+  const btnHomeToChurch = document.querySelector('#btnHomeToChurch');
+  const btnChurchToReception = document.querySelector('#btnChurchToReception');
   const btnSponsors = document.querySelector('#btnSponsors');
   const video = document.querySelector('#prototype-video');
 
@@ -40,8 +41,14 @@ function addInitialEventHandlers() {
     video.play();
   });
 
-  btnMap.addEventListener("click", event => {
-    console.log("btnMap clicked")
+  btnHomeToChurch.addEventListener("click", event => {
+    console.log("btnHomeToChurch clicked")
+    const directionLink = "https://www.google.com/maps?saddr=My+Location&daddr=Iglesia%20Ni%20Cristo%20-%20Lokal%20ng%20EVM%20-%20Cavite%20South,%204W5X+XM5,%20Tagaytay,%20Cavite";
+    window.open(directionLink, "_blank");
+  });
+
+  btnChurchToReception.addEventListener("click", event => {
+    console.log("btnChurchToReception clicked")
     const directionLink = "https://www.google.com/maps/dir/Iglesia+Ni+Cristo+-+Lokal+ng+EVM+-+Cavite+South,+4W5X%2BXM5,+Tagaytay,+Cavite/LieSil%E2%80%99s+Restaurant+%26+Events,+J.P.+Rizal+Street,+Kaybagal+South,+Tagaytay,+Cavite/@14.1056229,120.9392312,15z/data=!4m14!4m13!1m5!1m1!1s0x33bd7744d1349a79:0x922c3e148452585b!2m2!1d120.9492484!2d14.1098942!1m5!1m1!1s0x33bd77b43a3e77a3:0xeebb99ef08880fc4!2m2!1d120.9360291!2d14.102883!3e0?entry=ttu";
     window.open(directionLink, "_blank");
   });
