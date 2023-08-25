0e50f27861b86330d388a50d8bb12e92a50486d4
MrMayTwenty <30350668+MrMayTwenty@users.noreply.github.com>
2023-08-25 19:24:15 +0800

    Change base rotation of dancer, apply direction on current lat lng to Church

--- Scripts/EventHandler.js
+++ Scripts/EventHandler.js
@@ -67,7 +67,9 @@ function addInitialEventHandlers() {
 
 function showPosition(position) {
   //Use the position to load as the starting point for google maps
-  const directionLink = "https://www.google.com/maps?saddr=" + position.coords.latitude + "," + position.coords.longitude + "&daddr=Iglesia%20Ni%20Cristo%20-%20Lokal%20ng%20EVM%20-%20Cavite%20South,%204W5X+XM5,%20Tagaytay,%20Cavite";
+
+  //Format https://www.google.com/maps/dir/14.175415810227255,+120.96839002286967
+  const directionLink = "https://www.google.com/maps?saddr=" + position.coords.latitude + "," + position.coords.longitude + "/Iglesia+Ni+Cristo+-+Lokal+ng+EVM+-+Cavite+South,+Tagaytay,+Cavite/@14.1472351,120.9190007,13z/data=!3m1!4b1!4m13!4m12!1m5!1m1!1s0x0:0x62974b2fa15e7e1!2m2!1d120.9732917!2d14.1751663!1m5!1m1!1s0x33bd7744d1349a79:0x922c3e148452585b!2m2!1d120.9492484!2d14.1098942?entry=ttu";
   window.open(directionLink, "_blank");
 }
 
